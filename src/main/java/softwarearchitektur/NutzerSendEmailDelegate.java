3db87916bf70c768caf73b969c062d4d9d6d256e
Erik Damm <erik.damm11@googlemail.com>
2022-03-13 20:59:33 +0100

    add Nutzer prüfen

---
+++ src/main/java/softwarearchitektur/NutzerSendEmailDelegate.java
@@ -0,0 +1,17 @@
+package softwarearchitektur;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class NutzerSendEmailDelegate implements JavaDelegate{
+	
+    private static final String MAIL = "erik.damm97@googlemail.com";
+	
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+        String content = MailTemplate.mailTemplate;
+        content = content.replace("<#content>","Guten Tag, es tut uns leid Ihnen mitteilen zu müssen, dass Sie für das Leihen dieses Artikels nicht zugelassen sind.");
+        new MailService().sendEmail(MAIL,"Artikel-Verfügbarkeit", content);  
+	}
+
+}

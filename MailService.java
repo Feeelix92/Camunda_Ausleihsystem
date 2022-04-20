5665c027590755c3eefd3482fe72e227c3bb5ba1
Stefan-G33 <41584122+Stefan-G33@users.noreply.github.com>
2022-04-20 16:27:18 +0200

    Add files via upload

---
+++ MailService.java
@@ -0,0 +1,41 @@
+package softwarearchitektur;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.mail.*;
+
+
+
+public class MailService {
+
+	private static final String HOST = "smtp.googlemail.com";
+	private static final String USER = "iegruppe3testmail@gmail.com";
+	private static final String PWD = "qwertz!23";
+	private static final Integer PORT = 465;
+
+    public void sendEmail(String to, String subject, String content) throws EmailException {
+        
+        try {
+        	HtmlEmail email = new HtmlEmail();
+			email.setHostName(HOST);
+			email.setAuthentication(USER, PWD);
+	        email.setSmtpPort(PORT);
+	        email.setDebug(true);
+	        email.setSSLOnConnect(true);
+	        email.setHtmlMsg(content);
+	        email.setCharset("utf-8");
+
+	        // Just in case HTML is unsupported
+	        email.setTextMsg("Dein E-Mail-Client unterstützt kein HTML.");
+	        email.addTo(to);
+	        email.setFrom(USER);
+	        email.setSubject(subject);
+	        email.send();
+		} catch (EmailException e) {
+			e.printStackTrace();
+		}
+        
+    }
+}

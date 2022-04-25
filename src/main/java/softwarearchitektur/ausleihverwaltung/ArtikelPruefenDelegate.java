640f11123e83fdcb4351f7dcda99cd728a4e51f5
Erik Damm <erik.damm11@googlemail.com>
2022-04-25 16:02:27 +0200

    add ArtikelPruefenDelegate

---
+++ src/main/java/softwarearchitektur/ausleihverwaltung/ArtikelPruefenDelegate.java
@@ -0,0 +1,38 @@
+package softwarearchitektur.ausleihsystem;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.Variables.SerializationDataFormats;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArtikelPruefenDelegate implements JavaDelegate {
+	
+
+	public ArtikelPruefenDelegate() {
+
+	}
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		int artikelNr = (int) execution.getVariable("artikel");
+		
+		ArrayList<Integer> verfügbareArtikel = new ArrayList<Integer>();
+		verfügbareArtikel.add(1);
+		verfügbareArtikel.add(2);
+		
+		boolean verfügbar = false;
+			
+		verfügbar = verfügbareArtikel.contains(artikelNr);
+		
+		execution.setVariable("artikelNr", artikelNr);
+		execution.setVariable("artikel_verfuegbar", verfügbar);
+		
+	}
+
+}
